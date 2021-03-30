e47ab9de34ef330e78355575b678b7c850bad5ac
Daniel Hines <goldencube@protonmail.com>
2021-03-30 11:52:42 -0400

    wip

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 P=$(pwd)
 cd ../tezos
+rm .git/info/exclude
+rm .git/info/hooks/pre-push
+
+ln -s $P/hooks/pre-push .git/hooks/pre-push
 ln -s $P/exclude .git/info
 ln -s $P/.vscode $(pwd)
 ln -s $P/.envrc $(pwd)
