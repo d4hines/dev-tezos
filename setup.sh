8ccb89d1fec964180165c2c8010cc4bac5c39181
Daniel Hines <goldencube@protonmail.com>
2021-03-30 12:29:30 -0400

    wip

--- setup.sh
+++ setup.sh
@@ -1,10 +1,23 @@
+set -e
+
 P=$(pwd)
 cd ../tezos
-rm .git/info/exclude
-rm .git/info/hooks/pre-push
 
+echo "Adding hooks to local Git db..."
+rm -f .git/hooks/pre-push
 ln -s $P/hooks/pre-push .git/hooks/pre-push
+
+echo "Changing local Git excludes..."
+rm -f .git/info/exclude
 ln -s $P/exclude .git/info
+
+echo "Adding .vscode settings..."
+rm -rf .vscode
 ln -s $P/.vscode $(pwd)
+
+echo "Adding dir-env config..."
+rm -f .envrc
 ln -s $P/.envrc $(pwd)
+
+echo "Done! Enjoy hacking on Tezos 🚀"
 cd $P
